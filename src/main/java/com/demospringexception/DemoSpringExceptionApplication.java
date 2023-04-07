0c246ee214bde6dff3fff05e8b356d33bb4c3029
unknown <nguyentuan24072015@gmail.com>
2023-04-07 09:38:17 +0700

    upload to demo spring exception

---
+++ src/main/java/com/demospringexception/DemoSpringExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.demospringexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demospringexception/DemoSpringExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demospringexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
